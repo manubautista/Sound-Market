13d575941a0dd3b10877ae946a2ec2bffff531b1
manubautista <manubautista2009@gmail.com>
2022-08-17 14:24:10 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+It's an Ecommerce application with a content managging system and also complete payments integration.
+
+React Hooks (Front end)
+- Hooks
+- State Management with React Context API
+
+Next Js (Back end)
+- file based routing
+- Data fetching 
+- Server side rendering static generation
+
+Stripe
+- Payments
+- Products
+
+Sanity (Backoffice)
+- Content managging system 
---
+++ components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
+import Link from 'next/link';
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>2022 Sound Market All rights reserverd</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+      <p>Made With by Manuel Bautista</p>
+      <a className="email" target='_blank' href="https://manuelbautista.netlify.app/">
+        My Personal Portfolio 😉
+      </a>
+    </div>
+  )
+}
+
+export default Footer
